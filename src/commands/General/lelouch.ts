3ad28340a1948a9320882c9297df8cfb56a2280e
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 20:56:26 +0530

    Create lelouch.ts

---
+++ src/commands/General/lelouch.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "lelouch",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}elaina`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://ahad-cloud.mirror-index.workers.dev/0:/Uploads/video_2022-05-06_03-04-31_7094454011202374656.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: ` *Zero* \n\n🍀 *Description: repo is private better luck next time.*\n\n🌐 *insta id: https://www.instagram.com/akuma__24/?hl=en* \n\n 📒\n`,
+			}
+		);
+	};
+}

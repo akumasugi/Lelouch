3f292dcfe915011cd55bf915cfa455f592c978ec
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 21:51:08 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
             const n = [
-            'https://telegra.ph/file/9710964eeee092d1acec6.mp4'
+            'https://telegra.ph/file/9710964eeee092d1acec6.mp4','https://telegra.ph/file/72a30c81bfda485199fc9.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
 	if (!parsedArgs.joined) {

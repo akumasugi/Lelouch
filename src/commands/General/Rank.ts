79baaa96a4a167fecd81e6df2f89efdf66359f31
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-26 13:06:56 +0530

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -32,9 +32,9 @@ export default class Command extends BaseCommand {
 		try {
 			pfp = await this.client.getProfilePicture(user);
 		} catch (err) {
-			M.reply(`Profile Picture not Accessible of ${username}`);
+			M.reply(`Profile Picture not Accessible of ${username} must be ugly probably`);
 			pfp =
-				"https://cdn.donmai.us/original/ab/5e/ab5e2ec951546e5df432c9233ef9ab8a.jpg";
+				"https://i.ibb.co/pnQbYfw/Code-Geass-Hangyaku-no-Lelouch-Lelouch-Lamperouge-Ichiban-Kuji-Ichiban-Kuji-Code-Geass-Hangyaku-no-L.jpg";
 		}
 		const exp = (await this.client.getUser(user)).Xp;
 		let role: string;
